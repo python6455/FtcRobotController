61cb79e602b18b6e5b6ed763bd75ef849c1f9bc6
donboscorobotics <114185323+donboscorobotics@users.noreply.github.com>
2026-09-15 09:47:52 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmicronTeleop.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Omicron - Teleop", group = "Robot")
+public class OmicronTeleop extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop() {
+
+    }
+}
